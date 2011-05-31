80987b535cd8f2c6fb78d7a4896731320709df42
thomaswprice@msn.com <thomaswprice@msn.com@60e96b40-8f2e-fc84-2992-6de8df3f40ca>
2011-05-31 21:56:14 +0000

    Working Asteroids game

---
+++ Asteroids/src/com/twp/asteroids/Asteroids.java
@@ -0,0 +1,40 @@
+package com.twp.asteroids;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import edu.elon.honors.price.game.Game;
+import edu.elon.honors.price.game.Logic;
+import edu.elon.honors.price.graphics.GraphicsView;
+
+public class Asteroids extends Game {
+  
+	@Override
+	protected Logic getNewLogic() {
+		return new AsteroidsLogic();
+	}
+	
+	@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        menu.add("New Game");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case 0:
+        	AsteroidsLogic logic = (AsteroidsLogic)view.getLogic();
+        	view.setLogic(null);
+        	synchronized (logic) {
+        		logic.dispose();
+        		logic.setNewGame(true);
+        		logic.initialize();
+        	}
+        	view.setLogic(logic);
+        	break;
+        }
+
+        return false;
+    }
+}
