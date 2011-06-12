b2c63b963322b23dcac21afe616dbfff55b7c0e6
crowcasso <crowcasso@60e96b40-8f2e-fc84-2992-6de8df3f40ca>
2011-06-12 22:48:25 +0000

    first commit

---
+++ PhysicsLib/obj/local/armeabi/objs/box2d/Box2D/Common/b2BlockAllocator.o.d
@@ -0,0 +1,92 @@
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/obj/local/armeabi/objs/box2d/Box2D/Common/b2BlockAllocator.o: \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Common/b2BlockAllocator.cpp \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Common/b2BlockAllocator.h \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Common/b2Settings.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/assert.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/sys/cdefs.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/math.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/sys/types.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/stdint.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/sys/_types.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/machine/_types.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/linux/posix_types.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/linux/stddef.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/linux/compiler.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/asm/posix_types.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/asm/types.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/linux/types.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/machine/kernel.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/limits.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/sys/limits.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/linux/limits.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/machine/internal_types.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/machine/limits.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/sys/syslimits.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/stdlib.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/string.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/malloc.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/alloca.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/strings.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/memory.h
+
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Common/b2BlockAllocator.h:
+
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Common/b2Settings.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/assert.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/sys/cdefs.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/math.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/sys/types.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/stdint.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/sys/_types.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/machine/_types.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/linux/posix_types.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/linux/stddef.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/linux/compiler.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/asm/posix_types.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/asm/types.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/linux/types.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/machine/kernel.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/limits.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/sys/limits.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/linux/limits.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/machine/internal_types.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/machine/limits.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/sys/syslimits.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/stdlib.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/string.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/malloc.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/alloca.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/strings.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/memory.h:
