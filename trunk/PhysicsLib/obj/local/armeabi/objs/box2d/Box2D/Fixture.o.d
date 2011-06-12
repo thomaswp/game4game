1adaa977dcf5bebd5de3c5ae78374e8e52cd8ae4
crowcasso <crowcasso@60e96b40-8f2e-fc84-2992-6de8df3f40ca>
2011-06-12 22:48:25 +0000

    first commit

---
+++ trunk/PhysicsLib/obj/local/armeabi/objs/box2d/Box2D/Fixture.o.d
@@ -0,0 +1,188 @@
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/obj/local/armeabi/objs/box2d/Box2D/Fixture.o: \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Fixture.cpp \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Box2D.h \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Common/b2Settings.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/assert.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/sys/cdefs.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/math.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/sys/types.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/stdint.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/sys/_types.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/machine/_types.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/linux/posix_types.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/linux/stddef.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/linux/compiler.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/asm/posix_types.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/asm/types.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/linux/types.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/machine/kernel.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/limits.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/sys/limits.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/linux/limits.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/machine/internal_types.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/machine/limits.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/sys/syslimits.h \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Collision/Shapes/b2CircleShape.h \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Collision/Shapes/b2Shape.h \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Common/b2BlockAllocator.h \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Common/b2Math.h \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Collision/b2Collision.h \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Collision/Shapes/b2PolygonShape.h \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Collision/b2BroadPhase.h \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Collision/b2DynamicTree.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/stdlib.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/string.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/malloc.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/alloca.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/strings.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/memory.h \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Collision/b2Distance.h \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Collision/b2TimeOfImpact.h \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Dynamics/b2Body.h \
+ /Applications/eclipse/android-ndk-r5c/sources/cxx-stl/system/include/new \
+ /Applications/eclipse/android-ndk-r5c/sources/cxx-stl/system/include/cstddef \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Dynamics/b2Fixture.h \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Dynamics/b2WorldCallbacks.h \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Dynamics/b2TimeStep.h \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Dynamics/b2World.h \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Common/b2StackAllocator.h \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Dynamics/b2ContactManager.h \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Dynamics/Contacts/b2Contact.h \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Dynamics/Joints/b2DistanceJoint.h \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Dynamics/Joints/b2Joint.h \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Dynamics/Joints/b2FrictionJoint.h \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Dynamics/Joints/b2GearJoint.h \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Dynamics/Joints/b2LineJoint.h \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Dynamics/Joints/b2MouseJoint.h \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Dynamics/Joints/b2PrismaticJoint.h \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Dynamics/Joints/b2PulleyJoint.h \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Dynamics/Joints/b2RevoluteJoint.h \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Dynamics/Joints/b2WeldJoint.h \
+ /Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Fixture.h \
+ /Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/jni.h
+
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Box2D.h:
+
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Common/b2Settings.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/assert.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/sys/cdefs.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/math.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/sys/types.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/stdint.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/sys/_types.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/machine/_types.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/linux/posix_types.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/linux/stddef.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/linux/compiler.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/asm/posix_types.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/asm/types.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/linux/types.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/machine/kernel.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/limits.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/sys/limits.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/linux/limits.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/machine/internal_types.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/machine/limits.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/sys/syslimits.h:
+
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Collision/Shapes/b2CircleShape.h:
+
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Collision/Shapes/b2Shape.h:
+
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Common/b2BlockAllocator.h:
+
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Common/b2Math.h:
+
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Collision/b2Collision.h:
+
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Collision/Shapes/b2PolygonShape.h:
+
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Collision/b2BroadPhase.h:
+
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Collision/b2DynamicTree.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/stdlib.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/string.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/malloc.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/alloca.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/strings.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/memory.h:
+
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Collision/b2Distance.h:
+
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Collision/b2TimeOfImpact.h:
+
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Dynamics/b2Body.h:
+
+/Applications/eclipse/android-ndk-r5c/sources/cxx-stl/system/include/new:
+
+/Applications/eclipse/android-ndk-r5c/sources/cxx-stl/system/include/cstddef:
+
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Dynamics/b2Fixture.h:
+
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Dynamics/b2WorldCallbacks.h:
+
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Dynamics/b2TimeStep.h:
+
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Dynamics/b2World.h:
+
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Common/b2StackAllocator.h:
+
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Dynamics/b2ContactManager.h:
+
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Dynamics/Contacts/b2Contact.h:
+
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Dynamics/Joints/b2DistanceJoint.h:
+
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Dynamics/Joints/b2Joint.h:
+
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Dynamics/Joints/b2FrictionJoint.h:
+
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Dynamics/Joints/b2GearJoint.h:
+
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Dynamics/Joints/b2LineJoint.h:
+
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Dynamics/Joints/b2MouseJoint.h:
+
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Dynamics/Joints/b2PrismaticJoint.h:
+
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Dynamics/Joints/b2PulleyJoint.h:
+
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Dynamics/Joints/b2RevoluteJoint.h:
+
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Dynamics/Joints/b2WeldJoint.h:
+
+/Users/jhollingsworth/workspace/honors/price/PhysicsLib/jni/Box2D/Fixture.h:
+
+/Applications/eclipse/android-ndk-r5c/platforms/android-9/arch-arm/usr/include/jni.h:
