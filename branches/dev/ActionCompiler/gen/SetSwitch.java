bb461f6ad5d2c52f3cf6ae135d6ddbe76ab83574
thomaswprice@msn.com <thomaswprice@msn.com@60e96b40-8f2e-fc84-2992-6de8df3f40ca>
2012-10-01 22:55:24 +0000

    Sync

--- branches/dev/ActionCompiler/gen/SetSwitch.java
+++
@@ -1,8 +0,0 @@
-import edu.elon.honors.price.action.*;
-import edu.elon.honors.price.data.*;
-
-public class SetSwitch extends Action {
-	public static final String NAME = "Set Switch"
-	public static final int ID = 0
-	
-}
