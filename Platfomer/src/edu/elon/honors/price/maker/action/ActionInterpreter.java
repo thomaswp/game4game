c8d55ba356f1f76beb4ea386a23de1d4e1d99b80
thomaswprice@msn.com <thomaswprice@msn.com@60e96b40-8f2e-fc84-2992-6de8df3f40ca>
2012-10-04 14:03:54 +0000

    Testing an action selection method

--- Platfomer/src/edu/elon/honors/price/maker/action/ActionInterpreter.java
+++ Platfomer/src/edu/elon/honors/price/maker/action/ActionInterpreter.java
@@ -1,6 +1,6 @@
 package edu.elon.honors.price.maker.action;
 
-import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.util.HashMap;
 
@@ -8,31 +8,35 @@ import edu.elon.honors.price.data.Event.Action;
 import edu.elon.honors.price.game.Game;
 
 public abstract class ActionInterpreter<T extends ActionInstance> {
-	
+
 	private static final Class<?>[] interpreters = new Class<?>[] {
-			InterpreterDebugBox.class
+		InterpreterSetSwtich.class,
+		InterpreterDebugBox.class
 	};
-	
+
 	private static HashMap<Action, ActionInstance> actionMap =
 			new HashMap<Action, ActionInstance>();
-	
+
 	private static HashMap<Action, ActionInterpreter<?>> interperaterMap =
 			new HashMap<Action, ActionInterpreter<?>>();
-	
+
+//	private static HashMap<Action, Method> methodMap =
+//			new HashMap<Action, Method>();
+
 	protected Class<?> getActionClass() {
 		return getActionClass(getClass());
 	}
 
-	@SuppressWarnings({ "unchecked", "rawtypes" })
+	//@SuppressWarnings({ "unchecked", "rawtypes" })
 	private static Class<?> getActionClass(Class<?> cls) {
 		ParameterizedType parameterizedType =
 				(ParameterizedType) cls.getGenericSuperclass();
 		return (Class<?>) parameterizedType.getActualTypeArguments()[0];
 	}
-	
+
 	protected abstract void interperate(T action, PlatformGameState gameState) 
 			throws ParameterException;
-	
+
 	public static void interperate(Action action, PlatformGameState gameState) 
 			throws ParameterException {
 		if (!actionMap.containsKey(action)) {
@@ -44,7 +48,7 @@ public abstract class ActionInterpreter<T extends ActionInstance> {
 		}
 
 		ActionInstance instance = actionMap.get(action);
-		
+
 		if (!interperaterMap.containsKey(action)) {
 			ActionInterpreter<?> interp = null;
 			for (Class<?> cls : interpreters) {
@@ -52,7 +56,8 @@ public abstract class ActionInterpreter<T extends ActionInstance> {
 					try {
 						interp = (ActionInterpreter<?>)cls
 								.getConstructors()[0]
-								.newInstance();
+										.newInstance();
+						break;
 					} catch (Exception e) {
 						Game.debug(e);
 					}
@@ -62,8 +67,36 @@ public abstract class ActionInterpreter<T extends ActionInstance> {
 					"No interpreter for action #" + action.id);
 			interperaterMap.put(action, interp);
 		}
-		
+
 		ActionInterpreter<?> interp = interperaterMap.get(action);
-		//interp.getClass().getMethod("interperate", instance.getClass(), GameState.class);
+
+		invoke(instance.getClass(), interp, instance, gameState);
+
+		//		if (!methodMap.containsKey(action)) {
+		//			try {
+		//				Method method = interp.getClass().getMethod("interperate", 
+		//						instance.getClass(), GameState.class);
+		//				methodMap.put(action, method);
+		//				
+		//			} catch (Exception e) {
+		//				Game.debug(e);
+		//			}
+		//		}
+		//		
+		//		Method method = methodMap.get(action);
+		//		try {
+		//			method.invoke(interp, instance, gameState);
+		//		} catch (Exception e) {
+		//			Game.debug(e);
+		//		}
+	}
+
+	@SuppressWarnings("unchecked")
+	private static <S extends ActionInstance> void invoke(Class<S> cls, 
+			ActionInterpreter<?> interp, ActionInstance instance,
+			PlatformGameState gameState) throws ParameterException {
+		ActionInterpreter<S> castInterp = (ActionInterpreter<S>)interp;
+		S castAction = (S)instance;
+		castInterp.interperate(castAction, gameState);
 	}
 }
---
+++ Platfomer/src/edu/elon/honors/price/maker/action/InterpreterSetSwtich.java
@@ -0,0 +1,11 @@
+package edu.elon.honors.price.maker.action;
+
+public class InterpreterSetSwtich extends ActionInterpreter<ActionSetSwitch> {
+
+	@Override
+	protected void interperate(ActionSetSwitch action,
+			PlatformGameState gameState) throws ParameterException {
+		
+	}
+
+}
