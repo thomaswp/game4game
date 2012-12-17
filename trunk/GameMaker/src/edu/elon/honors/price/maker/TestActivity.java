3840ace6dad79b4c9b7b3ac102eddb523cb726e4
thomaswprice@msn.com <thomaswprice@msn.com@60e96b40-8f2e-fc84-2992-6de8df3f40ca>
2012-12-17 15:00:23 +0000

    Pre-Website

--- trunk/GameMaker/src/edu/elon/honors/price/maker/TestActivity.java
+++ trunk/GameMaker/src/edu/elon/honors/price/maker/TestActivity.java
@@ -1,47 +1,131 @@
 package edu.elon.honors.price.maker;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.StringWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import edu.elon.honors.price.game.Debug;
+import edu.elon.honors.price.game.Game;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.accounts.AccountManagerCallback;
+import android.accounts.AccountManagerFuture;
 import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Handler.Callback;
+import android.os.Message;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
 public class TestActivity extends DatabaseActivity {
-	
+
+	String token;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		LinearLayout layout = new LinearLayout(this);
-		layout.setBackgroundColor(Color.WHITE);
-		
-		ListView lv = new ListView(this);
-		layout.addView(lv);
-		ArrayList<String> items = new ArrayList<String>();
-		String[] itemsA =
-		"Hello this is a bunch of words and I think you should use them".split(" ");
-		for (String item : itemsA) items.add(item);
-		lv.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-		lv.setAdapter(new MyAdapter(this, R.layout.spinner_text, items));
-		
-		setContentView(layout);
-	}
-	
-	private static class MyAdapter extends CheckableArrayAdapter<String> {
 
-		public MyAdapter(Context context, int listItemResourceId,
-				ArrayList<String> items) {
-			super(context, listItemResourceId, items);
+		AccountManager am = AccountManager.get(this);
+		Account[] accounts = am.getAccountsByType("com.google");
+		for (Account a : accounts) {
+			Debug.write("%s: %s", a.name, a.type);
 		}
 
+		Bundle options = new Bundle();
+
+		am.getAuthToken(
+				accounts[0],                    // Account retrieved using getAccountsByType()
+				"ah",            				// Auth scope
+				options,                        // Authenticator-specific options
+				this,                           // Your activity
+				new OnTokenAcquired(),          // Callback called when a token is successfully acquired
+				new Handler(new OnError()));
+
+	}
+
+	private class OnError implements Callback {
 		@Override
-		protected void setRow(int position, String item, View view) {
-			((TextView)view).setText(item);
+		public boolean handleMessage(Message arg0) {
+			Debug.write("Error: %s", arg0);
+			return true;
 		}
-		
+
 	}
-	
+
+	private class OnTokenAcquired implements AccountManagerCallback<Bundle> {
+		@Override
+		public void run(AccountManagerFuture<Bundle> result) {
+			try {
+				// Get the result of the operation from the AccountManagerFuture.
+				Bundle bundle = result.getResult();
+
+				// The token is a named value in the bundle. The name of the value
+				// is stored in the constant AccountManager.KEY_AUTHTOKEN.
+				token = bundle.getString(AccountManager.KEY_AUTHTOKEN);
+				Debug.write(token);
+
+				Thread t = new Thread(new Runnable() {
+					@Override
+					public void run() {
+						try {
+							
+							
+							
+//							URL url = new URL("http://eujeux-test.appspot.com/eujeux");
+//							//URL url = new URL("http://www.google.com");
+//							URLConnection conn = (HttpURLConnection) url.openConnection();
+//
+//							conn.setRequestProperty("Authorization", "OAuth " + token);
+//
+//							Debug.write(conn.getContentLength());
+//							InputStream is = conn.getInputStream();
+//
+//							String out = convertStreamToString(is);
+//							Debug.write(out);
+							
+							HttpClient client = new DefaultHttpClient();
+							
+							HttpGet get = new HttpGet("https://eujeux-test.appspot.com/eujeux");
+							get.setHeader("Authorization", token);
+
+							HttpResponse resp = client.execute(get);
+							HttpEntity entity = resp.getEntity();
+							Debug.write(EntityUtils.toString(entity));
+
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				});
+				t.start();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static String convertStreamToString(java.io.InputStream is) {
+		java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+		return s.hasNext() ? s.next() : "";
+	}
+
 }
