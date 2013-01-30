856a49d736a23a9f0d548b63d239ab12087fae68
thomaswprice@msn.com <thomaswprice@msn.com@60e96b40-8f2e-fc84-2992-6de8df3f40ca>
2013-01-30 00:47:40 +0000

    Browsing implemented

---
+++ GameMaker/src/edu/elon/honors/price/maker/share/GameAdapter.java
@@ -0,0 +1,50 @@
+package edu.elon.honors.price.maker.share;
+
+import java.util.List;
+
+import com.eujeux.data.GameInfo;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ProgressBar;
+
+public class GameAdapter extends ArrayAdapter<GameInfo>{
+
+	private OnScrolledToBottomListener onScrolledToBottomListener;
+	private int lastLoadPosition = 0;
+
+	public void setOnScrolledToBottomListener(
+			OnScrolledToBottomListener onScrolledToBottomListener) {
+		this.onScrolledToBottomListener = onScrolledToBottomListener;
+	}
+
+	public GameAdapter(Activity context, List<GameInfo> objects) {
+		super(context, 0, objects);
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		
+
+		if (onScrolledToBottomListener != null) {
+			if (position == getCount() - 1 && lastLoadPosition < position) {
+				lastLoadPosition = position;
+				onScrolledToBottomListener.onScrolledToBottom();
+			}
+		}
+		
+		GameInfo info = getItem(position);
+		if (convertView == null) {
+			convertView = new WebGameView((Activity)getContext(), info);
+		} else {
+			((WebGameView)convertView).setGame(info);
+		}
+		return convertView;
+	}
+
+	public interface OnScrolledToBottomListener {
+		public void onScrolledToBottom();
+	}
+}
